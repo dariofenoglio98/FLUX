6d6a9c83b38db70ddab283f43f81c5c55ba12a91
dariofenoglio98 <139016178+dariofenoglio98@users.noreply.github.com>
2024-10-07 15:45:37 +0200

    nothing

--- public/config.py
+++ public/config.py
@@ -14,13 +14,13 @@ fedprox_proximal_mu = 0.001
 # Dataset settings
 dataset_name = "MNIST" # ["CIFAR10", "CIFAR100", "MNIST", "FMNIST", "EMNIST"]
 drifting_type = 'static' # ['static', 'trND_teDR', 'trDA_teDR', 'trDA_teND', 'trDR_teDR', 'trDR_teND'] refer to ANDA page for more details
-non_iid_type = 'label_skew' # refer to ANDA page for more details
+non_iid_type = 'feature_skew' # refer to ANDA page for more details
 show_features = True # show generated feature details if any
 show_labels = True # show distribution of data if any
 # careful with the args applying to your settings above
 args = {
-    'scaling_label_low':0.5,
-    'scaling_label_high':0.9
+    # 'scaling_label_low':0.5,
+    # 'scaling_label_high':0.9
 }
 
 # Training model settings
@@ -28,7 +28,7 @@ model_name = "LeNet5"   # ["LeNet5", "ResNet9"]
 batch_size = 64
 test_batch_size = 64
 client_eval_ratio = 0.2
-n_rounds = 10
+n_rounds = 5
 local_epochs = 2
 lr = 0.005
 momentum = 0.9
